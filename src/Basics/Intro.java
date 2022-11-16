c417872979c8de9717874a0921c9e643771874e2
Tanmay Arya <tanmayarya80@gmail.com>
2022-11-16 20:10:59 +0530

    Basics of Programming

---
+++ src/Basics/Intro.java
@@ -0,0 +1,31 @@
+package Basics;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("1");
+		System.out.print("2sdfasdasdas");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		
+		
+		int num = 10;
+//		int Num = 20;
+		System.out.println(num);
+		num = 30;
+		System.out.println(num);
+		
+		boolean var = true;
+		var = false;
+		
+		int a = num + 40;
+		System.out.println(a);
+		a = a - 10;
+		System.out.println(a);
+	}
+	
+
+}
